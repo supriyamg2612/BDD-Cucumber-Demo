195212fb0ac0b3f8c40bfa203fae784dba11d498
Supriya Ghuge <supriya@Ameyas-MBP.fritz.box>
2025-06-22 19:42:00 +0200

    Add GoogleSearchStepDef class with glue code for google search scenario
    - Step definitions for navigating to Google search page
    - Entering text in search box and hitting enter
    - Verifying navigation to search results page

---
+++ cucumber-demo/src/test/java/com/stepdefinition/GoogleSearchStepDef.java
@@ -0,0 +1,31 @@
+package com.stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSearchStepDef {
+	@Given("user is on google search page")
+	public void user_is_on_google_search_page() {
+	   
+	}
+
+	@When("user enters a text in search box")
+	public void user_enters_a_text_in_search_box() {
+	    
+	}
+
+	@When("hits enter")
+	public void hits_enter() {
+	  
+	}
+
+	@Then("user is navigated to search results")
+	public void user_is_navigated_to_search_results() {
+	   
+	}
+
+
+
+
+}
